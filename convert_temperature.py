675c9c471c6cb10c7f61154f62aa909cf24b772f
niketagrawal <15953349+niketagrawal@users.noreply.github.com>
2023-05-25 14:32:48 +0200

    calculation updated as per latest reference

--- convert_temperature.py
+++ convert_temperature.py
@@ -9,5 +9,5 @@ def fahrenheit_to_celsius(temperature_fahrenheit):
 	temperature_celsius
 
 	"""
-	temperature_celsius = (temperature_fahrenheit - 32) * (5.0/9.0)
+	temperature_celsius = (temperature_fahrenheit - 32) - (5.0/9.0)
 	return temperature_celsius
