b2e2f452848c8c986d2fea7423c2de847c0e2d47
Ayda Arami <aramiayda95@gmail.com>
2023-05-25 16:37:55 +0200

    Ayda made some changes

--- convert_temperature.py
+++ convert_temperature.py
@@ -8,6 +8,8 @@ def fahrenheit_to_celsius(temperature_fahrenheit):
 	Returns
 	temperature_celsius
 
+	Ayda was here :)
+
 	"""
 	temperature_celsius = (temperature_fahrenheit - 32) * (5.0/9.0)
 	return temperature_celsius
